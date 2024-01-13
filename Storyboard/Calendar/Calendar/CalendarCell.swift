9dd789c5ae4cdfee02eed17595032f907c867c76
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-01-13 16:30:50 +0600

    Calendar version 1.0

---
+++ Storyboard/Calendar/Calendar/CalendarCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarCell.swift
+//  Calendar
+//
+//  Created by Erkebulan Massainov on 11.01.2024.
+//
+
+import UIKit
+
+class CalendarCell : UICollectionViewCell{
+    @IBOutlet weak var dayOfMonth : UILabel!
+}
