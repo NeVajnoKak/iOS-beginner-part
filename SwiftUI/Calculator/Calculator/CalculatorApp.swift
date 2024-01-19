09fd27ac4f6893e303c82a19658e0a7fb4d35bf4
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-01-19 15:41:31 +0600

    add Calculator version 1.0 UI

---
+++ SwiftUI/Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Erkebulan Massainov on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
