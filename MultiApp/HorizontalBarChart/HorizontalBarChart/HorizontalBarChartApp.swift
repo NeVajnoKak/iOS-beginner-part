0dddda558f04006ab250e0c0fe904c41f662ba64
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-01-23 00:18:39 +0600

    HorizontalChart add version 1.0

---
+++ MultiApp/HorizontalBarChart/HorizontalBarChart/HorizontalBarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalBarChartApp.swift
+//  HorizontalBarChart
+//
+//  Created by Erkebulan Massainov on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalBarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
