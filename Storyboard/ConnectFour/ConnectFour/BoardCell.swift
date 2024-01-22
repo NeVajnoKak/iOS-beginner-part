d2d88b0eea61fb34e53042023585141634a4479d
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-01-22 23:42:37 +0600

    ConnectorFour game version 1.0

---
+++ Storyboard/ConnectFour/ConnectFour/BoardCell.swift
@@ -0,0 +1,14 @@
+//
+//  BoardCell.swift
+//  ConnectFourTutorial
+//
+//  Created by Erkebulan Massainov on 22.01.2024.
+//
+
+import UIKit
+
+class BoardCell : UICollectionViewCell
+{
+    
+    @IBOutlet weak var image: UIImageView!
+}
